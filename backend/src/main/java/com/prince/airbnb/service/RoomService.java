3e1978a4d6405cc7be2aba58792eb56af3164fd5
Prince Kumar <princek801301@gmail.com>
2026-08-30 13:25:31 +0530

    add room service and admin endpoints with inventory generation

---
+++ backend/src/main/java/com/prince/airbnb/service/RoomService.java
@@ -0,0 +1,19 @@
+package com.prince.airbnb.service;
+
+
+
+import com.prince.airbnb.dto.RoomDto;
+
+import java.util.List;
+
+public interface RoomService {
+
+    RoomDto createNewRoom(Long hotelId, RoomDto roomDto);
+
+    List<RoomDto> getAllRoomsInHotel(Long hotelId);
+
+    RoomDto getRoomById(Long roomId);
+
+    void deleteRoomById(Long roomId);
+
+}
