ef1b7b6c134aeaa8bde9a094742675f8429f7975
Prince Kumar <princek801301@gmail.com>
2026-09-04 17:44:33 +0530

    fix booking guests endpoint path

---
+++ backend/src/main/java/com/prince/airbnb/controller/HotelBookingController.java
@@ -0,0 +1,32 @@
+package com.prince.airbnb.controller;
+
+import com.prince.airbnb.dto.BookingDto;
+import com.prince.airbnb.dto.BookingRequest;
+import com.prince.airbnb.dto.GuestDto;
+import com.prince.airbnb.service.BookingService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bookings")
+public class HotelBookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping("/init")
+    public ResponseEntity<BookingDto> initialiseBooking(@RequestBody BookingRequest bookingRequest) {
+        return ResponseEntity.ok(bookingService.initialiseBooking(bookingRequest));
+    }
+
+    @PostMapping("/{bookingId}/addGuests")
+    public ResponseEntity<BookingDto> addGuests(@PathVariable Long bookingId,
+                                                @RequestBody List<GuestDto> guestDtoList) {
+        return ResponseEntity.ok(bookingService.addGuests(bookingId, guestDtoList));
+    }
+
+}
