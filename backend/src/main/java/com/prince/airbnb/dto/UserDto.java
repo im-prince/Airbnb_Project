4b9e0b3ff41949b3f4b3395038c9ec4866aae9c9
Prince Kumar <princek801301@gmail.com>
2026-09-05 13:48:36 +0530

    add JWT-based authentication and authorization

---
+++ backend/src/main/java/com/prince/airbnb/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.prince.airbnb.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String email;
+    private String name;
+}
---
+++ backend/src/main/java/com/prince/airbnb/service/UserService.java
@@ -0,0 +1,10 @@
+package com.prince.airbnb.service;
+
+
+import com.prince.airbnb.entity.User;
+
+public interface UserService {
+
+    User getUserById(Long id);
+
+}
